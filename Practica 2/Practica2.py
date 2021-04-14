672a3247b806d3713775429decb36af69e1dc64c
Mario Blanco Dominguez <56127361+running004@users.noreply.github.com>
2021-04-14 15:24:00 +0200

    P2 fallan los thetas :(

--- Practica 2/Practica2.py
+++ Practica 2/Practica2.py
@@ -29,7 +29,7 @@ from pandas.io.parsers import read_csv
 import scipy.optimize as opt
 
 def load_data(file_name):
-    return read_csv(file_name, header=None).to_numpy()
+    return read_csv(file_name, header=None).values.astype(float)
 
 
 def print_data(X, Y):
@@ -38,7 +38,7 @@ def print_data(X, Y):
     plt.scatter(X[pos, 0], X[pos, 1], marker='+', c='k')
     pos = np.where(Y == 0)
     plt.scatter(X[pos, 0], X[pos, 1], marker='x', c='b')
-    plt.show()
+    #plt.show()
     plt.savefig("data.pdf")
 
 
@@ -58,16 +58,50 @@ def gradiente(theta, X, Y):
     grad = (1 / len(Y)) * np.matmul(X.T, H - Y)
     return grad
 
+def pinta_frontera_recta(X, Y, theta):
+    x1_min, x1_max = X[:, 0].min(), X[:, 0].max()
+    x2_min, x2_max = X[:, 1].min(), X[:, 1].max()
+
+    xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max),
+    np.linspace(x2_min, x2_max))
+
+    h = sigmoide(np.c_[np.ones((xx1.ravel().shape[0], 1)),
+    xx1.ravel(),
+    xx2.ravel()].dot(theta))
+    h = h.reshape(xx1.shape)
+
+    # el cuarto parámetro es el valor de z cuya frontera se
+    # quiere pintar
+    plt.contour(xx1, xx2, h, [0.5], linewidths=2, colors='green')
+
+def porcentaje_aciertos(X, Y, theta):
+    aciertos = 0
+    j = 0
+    for i in X:
+        prod = sigmoide(np.dot(i, theta))
+        if prod >= 0.5 and Y[j] == 1:
+            aciertos += 1
+        elif prod < 0.5 and Y[j] == 0:
+            aciertos += 1
+    return aciertos / len(Y) * 100
 
 if __name__ == '__main__':
     datos = load_data('./ex2data1.csv')
-    X = datos[:, :2]
+    X = datos[:, :-1]
+
     Y = datos[:, -1]
-    print(datos)
+
+
     print_data(X, Y)
     theta = np.zeros(3)
-    m = np.shape(X)
+    m = np.shape(X)[0]
+    n = np.shape(X)[1]
+
+    X = np.hstack([np.ones([m, 1]), X])
+    nX = datos[:, :-1]
     result = opt.fmin_tnc(func=coste, x0=theta, fprime=gradiente, args=(X, Y))
     theta_opt = result[0]
-    print("x")
+    pinta_frontera_recta(nX, Y, theta_opt)
+    plt.show()
+    print("Prediccion con un porcentaje de aciertos de:", porcentaje_aciertos(X, Y, theta_opt))
 
