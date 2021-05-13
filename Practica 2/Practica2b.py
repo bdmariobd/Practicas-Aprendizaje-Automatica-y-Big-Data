a055ae3f833ca07748826b8aa7f55157d8d3f95c
JuanTecedor <43722131+JuanTecedor@users.noreply.github.com>
2021-05-14 01:14:29 +0200

    Parte 2 de la P2

--- Practica 2/Practica2b.py
+++ Practica 2/Practica2b.py
@@ -25,6 +25,7 @@
 # Obtiene un vector con los índices de los ejemplos positivos
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 import sklearn.preprocessing
 from pandas.io.parsers import read_csv
 import scipy.optimize as opt
@@ -39,52 +40,62 @@ def print_data(X, Y):
     # Dibuja los ejemplos positivos
     plt.scatter(X[pos, 0], X[pos, 1], marker='+', c='k')
     pos = np.where(Y == 0)
-    plt.scatter(X[pos, 0], X[pos, 1], marker='x', c='b')
-    #plt.show()
+    plt.scatter(X[pos, 0], X[pos, 1], marker='o', c='b')
     plt.savefig("data.pdf")
+    plt.legend(
+        handles=[
+            mpatches.Patch(color='black', label='y = 1'),
+            mpatches.Patch(color='blue', label='y = 0')
+        ])
 
 
-def sigmoide(z):
-    return 1 / (1 + np.exp(-z))
+def sigmoide(Z):
+    return 1 / (1 + np.exp(-Z))
 
 
 def coste(theta, X, Y, l):
-    m = len(X)
-    H = sigmoide(np.matmul(X, theta))
-    cost = (- 1 / m) * (np.dot(Y, np.log(H)) + np.dot((1 - Y), np.log(1 - H)))
-    return cost
+    m = np.shape(X)[0]
+    H = sigmoide(np.matmul(X, np.transpose(theta)))
+    
+    l1 = np.transpose(np.log(H))
+    l2 = np.transpose(np.log(1 - H))
+
+    ret = (-1 / m) * ((np.matmul(l1, Y)) + (np.matmul(l2, (1 - Y))))
+    return ret + l / (2 * m) * np.sum(H * H)
 
 
 def gradiente(theta, X, Y, l):
-    m = len(X)
-    H = sigmoide(np.matmul(X, theta))
-    grad = (1 / m) * np.matmul(X.T, H - Y)
-    return grad + (l/m) * theta
+    m = np.shape(X)[0]
+    H = sigmoide(np.matmul(X, np.transpose(theta)))
+    ret = (1 / m) * np.matmul(np.transpose(X), H - Y)
+    return ret + (l / m) * theta
 
 
 def plot_decisionboundary(X, Y, theta, poly):
-    plt.figure()
     x1_min, x1_max = X[:, 0].min(), X[:, 0].max()
     x2_min, x2_max = X[:, 1].min(), X[:, 1].max()
     xx1, xx2 = np.meshgrid(np.linspace(x1_min, x1_max),
-    np.linspace(x2_min, x2_max))
+    	np.linspace(x2_min, x2_max))
     h = sigmoide(poly.fit_transform(np.c_[xx1.ravel(),
-    xx2.ravel()]).dot(theta))
+    	xx2.ravel()]).dot(theta))
     h = h.reshape(xx1.shape)
     plt.contour(xx1, xx2, h, [0.5], linewidths=1, colors='g')
     plt.savefig("boundary.pdf")
-    plt.close()
 
-def porcentaje_aciertos(X, Y, theta): #
+
+def porcentaje_aciertos(X, Y, theta):
     aciertos = 0
     j = 0
-    for i in X:
-        prod = sigmoide(np.dot(i, theta))
-        if prod >= 0.5 and Y[j] == 1:
+    print(X, theta)
+    for i in range(len(X)):
+        pred = sigmoide(np.dot(X[i], theta))
+        if pred >= 0.5 and Y[j] == 1:
             aciertos += 1
-        elif prod < 0.5 and Y[j] == 0:
+        elif pred < 0.5 and Y[j] == 0:
             aciertos += 1
+        j += 1
     return aciertos / len(Y) * 100
+
 
 if __name__ == '__main__':
     datos = load_data('./ex2data2.csv')
@@ -96,12 +107,15 @@ if __name__ == '__main__':
     mapFeature = sklearn.preprocessing.PolynomialFeatures(6)
     mapFeatureX = mapFeature.fit_transform(X)
     theta = np.zeros(mapFeatureX.shape[1])
-    l=0
+    l=1
+    
     result = opt.fmin_tnc(func=coste, x0=theta, fprime=gradiente, args=(mapFeatureX, Y,l))
+    print(result)
     theta_opt = result[0]
-    data = np.delete(mapFeatureX,0,axis=1)
+    data = np.delete(mapFeatureX, 0, axis=1)
 
     plot_decisionboundary(data, Y, theta_opt,mapFeature)
     plt.show()
-    #print("Prediccion con un porcentaje de aciertos de:", porcentaje_aciertos(X, Y, theta_opt))
+    print("Prediccion con un porcentaje de aciertos de:",
+        porcentaje_aciertos(mapFeatureX, Y, theta_opt))
 
