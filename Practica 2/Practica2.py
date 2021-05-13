1a8f8381f23f62d38e181e5f90a1d97e42d5047b
JuanTecedor <43722131+JuanTecedor@users.noreply.github.com>
2021-05-14 00:26:28 +0200

    Arreglados errores

--- Practica 2/Practica2.py
+++ Practica 2/Practica2.py
@@ -25,6 +25,7 @@
 # Obtiene un vector con los índices de los ejemplos positivos
 import numpy as np
 import matplotlib.pyplot as plt
+import matplotlib.patches as mpatches
 from pandas.io.parsers import read_csv
 import scipy.optimize as opt
 
@@ -37,26 +38,35 @@ def print_data(X, Y):
     # Dibuja los ejemplos positivos
     plt.scatter(X[pos, 0], X[pos, 1], marker='+', c='k')
     pos = np.where(Y == 0)
-    plt.scatter(X[pos, 0], X[pos, 1], marker='x', c='b')
+    plt.scatter(X[pos, 0], X[pos, 1], marker='o', c='b')
     #plt.show()
     plt.savefig("data.pdf")
+    plt.legend(
+        handles=[
+            mpatches.Patch(color='black', label='Accepted'),
+            mpatches.Patch(color='blue', label='Refused')
+        ])
 
 
-def sigmoide(z):
-    return 1 / (1 + np.exp(-z))
+def sigmoide(Z):
+    return 1 / (1 + np.exp(-Z))
 
 
 def coste(theta, X, Y):
-    m = len(X)
-    H = sigmoide(np.matmul(X, theta))
-    cost = (- 1 / m) * (np.dot(Y, np.log(H)) + np.dot((1 - Y), np.log(1 - H)))
-    return cost
+    m = np.shape(X)[0]
+    H = sigmoide(np.matmul(X, np.transpose(theta)))
+    
+    l1 = np.transpose(np.log(H))
+    l2 = np.transpose(np.log(1 - H))
+
+    return (-1 / m) * ((np.matmul(l1, Y)) + (np.matmul(l2, (1 - Y))))
 
 
 def gradiente(theta, X, Y):
-    H = sigmoide(np.matmul(X, theta))
-    grad = (1 / len(Y)) * np.matmul(X.T, H - Y)
+    H = sigmoide(np.dot(X, np.transpose(theta)))
+    grad = (1 / np.shape(X)[0]) * np.matmul(np.transpose(X), H - Y)
     return grad
+
 
 def pinta_frontera_recta(X, Y, theta):
     x1_min, x1_max = X[:, 0].min(), X[:, 0].max()
@@ -74,7 +84,8 @@ def pinta_frontera_recta(X, Y, theta):
     # quiere pintar
     plt.contour(xx1, xx2, h, [0.5], linewidths=2, colors='green')
 
-def porcentaje_aciertos(X, Y, theta): #
+
+def porcentaje_aciertos(X, Y, theta):
     aciertos = 0
     j = 0
     for i in X:
@@ -85,22 +96,34 @@ def porcentaje_aciertos(X, Y, theta): #
             aciertos += 1
     return aciertos / len(Y) * 100
 
+
 if __name__ == '__main__':
     datos = load_data('./ex2data1.csv')
     X = datos[:, :-1]
-
     Y = datos[:, -1]
 
     print_data(X, Y)
-    theta = np.zeros(3)
+    
+    X = np.hstack([np.ones([np.shape(X)[0], 1]), X])
     m = np.shape(X)[0]
     n = np.shape(X)[1]
+    
+    theta = np.zeros(n)
 
-    X = np.hstack([np.ones([m, 1]), X])
+    print('1.3. Calculo: ', str(coste(theta, X, Y)), str(gradiente(theta, X, Y)))
+
     nX = datos[:, :-1]
     result = opt.fmin_tnc(func=coste, x0=theta, fprime=gradiente, args=(X, Y))
+    
+    print('Resultado opt.fmin_tnc: ' + str(result))
+    
     theta_opt = result[0]
+    
+    print('Coste final: ' + str(coste(theta_opt, X, Y)))
+
     pinta_frontera_recta(nX, Y, theta_opt)
-    plt.show()
+    
     print("Prediccion con un porcentaje de aciertos de:", porcentaje_aciertos(X, Y, theta_opt))
+    
+    plt.show()
 
