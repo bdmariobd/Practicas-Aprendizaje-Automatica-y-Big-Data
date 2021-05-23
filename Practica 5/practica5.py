616b81ca420ef49dc5f82e0e23fc0df6b0dd3e63
Mario Blanco Dominguez <56127361+running004@users.noreply.github.com>
2021-05-23 23:56:50 +0200

    Apartado 2 cas

--- Practica 5/practica5.py
+++ Practica 5/practica5.py
@@ -64,6 +64,18 @@ def gradient(X, Y, l, T):
 def costAndGrad(X, Y, T, l):
 	return cost(T, X, Y, l), gradient(X, Y, l, T)
 
+def get_errors(X,Y, Xval, Yval,l):
+    train_errors =  []
+    validation_errors = []
+    m= X.shape[0]
+    for i in range(1,m):
+        T = np.zeros(X.shape[1])
+        thetas = opt.minimize(fun=cost, x0=T, args=(X[0:i],Y[0:i],l)).x
+        train_errors.append(cost(thetas,X[0:i],Y[0:i],l))
+        validation_errors.append(cost(thetas,Xval,Yval,l))
+        
+    return (train_errors ,validation_errors)
+
 def main():
     data = io.loadmat('./ex5data1.mat')
     X, Y = data['X'], data['y']
@@ -77,7 +89,6 @@ def main():
     print(costAndGrad(X_ones, Y, T, l))
     res = (opt.minimize(fun=cost, x0=T, args=(X_ones, Y, 0))).x
     
-    plt.plot(X,Y,'x')
     theta_0, theta_1 = res[0], res[1]
     x = np.linspace(min(X), max(X), 100)
     y = theta_0 + theta_1 * x
@@ -87,7 +98,20 @@ def main():
     plt.ylabel('Water flowing out of the dam\'s')
 	# plt.savefig('result.png')
     plt.show()
-	# y = hypothesis(X, res)
+    
+    train_errors ,value_errors = get_errors(X, Y, Xval, Yval, 0)
+    
+    x = np.linspace(1, 11, 11)
+    plt.plot(x,train_errors, label='Train')
+    x = np.linspace(1, 11, 11)
+    plt.plot(x,value_errors, label='CrossVal')
+    plt.legend()
+    plt.suptitle('Learning curve for linear regression')
+    plt.xlabel('Number of training examples')
+    plt.ylabel('Error')
+    plt.show()
+    
+    
 
 
     
